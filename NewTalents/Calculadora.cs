f44e7955f900dcd86595ebb24941bbf66194d53b
Triz <triz.marciano@gmail.com>
2024-11-11 23:00:03 -0300

    TestCalculadora

---
+++ NewTalents/Calculadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTalents
+{
+    public class CalculadoraImp
+    {
+        private List<string> listahistorico;
+
+        public CalculadoraImp()
+        {
+            listahistorico = new List<string>();
+        }
+        public int Somar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            listahistorico.Insert(0, "Resultado: " + resultado);
+            return resultado;
+        }
+        public int Subtrair(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            listahistorico.Insert(0, "Resultado: " + resultado);
+            return resultado;
+        }
+        public int Multiplicar(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            listahistorico.Insert(0, "Resultado: " + resultado);
+            return resultado;
+        }
+        public int Dividir(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            listahistorico.Insert(0, "Resultado: " + resultado);
+            return resultado;
+        }
+        public List<string> Historico()
+        {
+            listahistorico.RemoveRange(3, listahistorico.Count - 3);
+            return listahistorico;
+        }
+    }
+}
